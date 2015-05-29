1b6e4ff9ebd02b4d25db4c72a019ce7d10f9d1da
Halo Lee <matrixlmsly@gmail.com>
2015-05-29 22:16:29 +1000

    Code clean up This is a bare minimum version of load data from Restful API to a tableView list

--- GoogleBlogsReader/MasterViewController.h
+++ GoogleBlogsReader/MasterViewController.h
@@ -7,13 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <CoreData/CoreData.h>
 
-@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
-
-@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
-@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-
+@interface MasterViewController : UITableViewController
 
 @end
 
