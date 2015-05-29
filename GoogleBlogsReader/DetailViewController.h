09b5c4f129779282eb90b2048c3233dacc7055f7
Halo Lee <matrixlmsly@gmail.com>
2015-05-29 16:09:19 +1000

    Initial Commit

---
+++ GoogleBlogsReader/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  GoogleBlogsReader
+//
+//  Created by Hao Li on 29/05/2015.
+//  Copyright (c) 2015 halolee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ GoogleBlogsReader/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  GoogleBlogsReader
+//
+//  Created by Hao Li on 29/05/2015.
+//  Copyright (c) 2015 halolee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
